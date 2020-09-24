7264c15f133e591838aebcd2bae0f0015f3eec07
smoore92 <Shane_Moore@knights.ucf.edu>
2020-09-24 15:46:01 -0400

    Animation commit

--- Tutorial 1.5/Assets/Scripts/Audio.cs
+++ Tutorial 1.5/Assets/Scripts/Audio.cs
@@ -7,11 +7,12 @@ public class Audio : MonoBehaviour
     public AudioSource musicSource;
     public AudioClip musicClipOne;
     public AudioClip musicClipTwo;
+    Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,19 +23,23 @@ public class Audio : MonoBehaviour
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          anim.SetInteger("State",1);
          }
         if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
+          anim.SetInteger("State",0);
          }
          if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State",2);
          }
         if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          anim.SetInteger("State",0);
          }
         if (Input.GetKeyDown(KeyCode.L))
         {
